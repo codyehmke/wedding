71769c74457c0319fe0a07a1a3d6103fc5cc3e0e
Cody Ehmke <69863503+codyehmke@users.noreply.github.com>
2022-07-25 20:13:14 -0700

    Update and rename home.html to index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+	include_once 'header.php'
+?>
+
+<div>
+	<img src="img/us7.jpg" class="us7">
+</div>
+
+	<div class="secondbase">
+		<div class="hometxt" style="color: #00405C";>
+			<div>
+				<p>Thank you for being a friend of ours, 
+					we are so blessed and grateful to have so many wonderful friends in our lives.
+					We hope to at the very least return the favor. As we join hands in marriage we
+					also hope to never fail to extend our hands to you and be there for you in need.
+					Thank you, for being there for us and lifting our spirits throughout our lives!</p>
+			</div>
+		</div>
+	</div>
+	<div class="thirdbase">
+		<div class="infotxt">
+			<div>
+				<h1 id="infotitle">Time</h1>
+				<h2>October 1st, Saturday</h2>
+				<h2>1:30pm-5:00pm</h2>
+			</div>
+			<br><br>
+			<div>
+				<h1 id="infotitle">Location</h1>
+				<h2>Wylie Northeast Church</h2>
+				<h2>100 W Oak st, Wylie</h2>
+			</div>
+		</div>
+	</div>
+	<div class="hometxt" style="color: #00405C";>
+		<div>
+			<div>
+				<img src="img/us3.jpg" class="us7">
+			<p>We are excited to see you!</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="secondbase">
+	<div class="rsvp-form">
+			<h1 class="formtitle">RSVP to our wedding!</h1>
+		<form class="form" action="">
+			<label class="form-label" for="yourname">Your first and last name</label>
+			<input class="form-text" id="yourname" name="yourname">
+			<br>
+			<label class="form-label" for="attendees">Additional attendees</label>
+			<input class="form-text" id="attendees" name="attendees">
+			<br>
+			<button class="button" style="color:aliceblue">Accept invitation</button>
+			<a href="#"></a>
+			<button class="button" style="color:aliceblue">Wont be able to make it</button>
+			<a href="#"></a>
+		</form>	
+	</div>
+
+<?php
+	include_once 'footer.php'
+?>
